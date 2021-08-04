39cae9e171f3315a002becf30f949a9330b67cdf
andrew adams <andrew.adamsgutierrez@gmail.com>
2021-08-04 10:45:00 -0500

    add json file with contents

--- functions/charge-card.js
+++ functions/charge-card.js
@@ -1,25 +1,11 @@
-const fs = require('fs');
-const matter = require('gray-matter');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
+const products = require ('./products,json');
 
-const getProducts = () => {
-    const directory = `${process.cwd()}/content`;
-    const filenames = fs.readdirSync(directory);
-    const products = filenames.map(filename => {
-      const fileContent = fs.readFileSync(`${directory}/${filename}`).toString();  // read the file from file system
-      const { data } = matter(fileContent) //pull out front matter => name
-    return data;
-    });
-
-    return products;
-
-}
 
 exports.handler = async (event, context) => {  
 const { cart } = JSON.parse(event.body);
 
-const products = getProducts();
 
 const cartWithProducts = cart.map(({ id, qty }) => {
     const product = products.find(p => p.id === id)
